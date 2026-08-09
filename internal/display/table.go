3ab1e411ef90268b6daf695a220e94c8be3d1369
KrishnaChaitanya20 <krishnachaitanyadevunoori@gmail.com>
2026-08-09 20:04:38 +0530

    Reorder columns for better readability in List Instances

--- internal/display/table.go
+++ internal/display/table.go
@@ -15,17 +15,17 @@ func PrintInstancesTable(instances []ec2client.Ec2Instances) {
 		return
 	}
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(w, "Instance ID\tInstance Type\tState\tPublic IP\tPrivate IP\tName\tUptime")
-	fmt.Fprintln(w, "-----------\t-------------\t-----\t---------\t----------\t----\t------")
+	fmt.Fprintln(w, "Instance ID\tName\tInstance Type\tPublic IP\tPrivate IP\tState\tUptime")
+	fmt.Fprintln(w, "-----------\t----\t-------------\t---------\t----------\t-----\t------")
 
 	for _, instance := range instances {
 		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
 			instance.InstanceID,
+			instance.InstanceName,
 			instance.InstanceType,
-			instance.Status,
 			instance.PublicIP,
 			instance.PrivateIP,
-			instance.InstanceName,
+			instance.Status,
 			instance.Uptime,
 		)
 	}
